722b7a2db59beea02373a45ae553b5bb6b87034b
Nishek Sharma <sharmanishek@yahoo.com>
2026-02-12 10:33:15 +0530

    feat(blog-controller): add create and fetch blog by slug endpoints

--- backend/blog-backend/src/main/java/com/nsdev/blog/module/blog/controller/AdminBlogController.java
+++ backend/blog-backend/src/main/java/com/nsdev/blog/module/blog/controller/AdminBlogController.java
@@ -1,5 +1,37 @@
 package com.nsdev.blog.module.blog.controller;
 
-public class AdminBlogController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.nsdev.blog.module.blog.dto.BlogRequestDto;
+import com.nsdev.blog.module.blog.dto.BlogResponseDto;
+import com.nsdev.blog.module.blog.service.BlogService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/admin/blogs")
+@RequiredArgsConstructor
+public class AdminBlogController {
+	
+	private final BlogService blogService;
+	
+	@PostMapping
+	public ResponseEntity<BlogResponseDto> createBlog(@Valid @RequestBody BlogRequestDto blogRequestDto) {
+		BlogResponseDto responseDto = blogService.createBlog(blogRequestDto);
+		return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/slug/{slug}")
+	public ResponseEntity<BlogResponseDto> getBlogBySlug(@PathVariable String slug) {
+		BlogResponseDto blogBySlug = blogService.getBlogBySlug(slug);
+		return ResponseEntity.ok(blogBySlug);
+	}
 }
