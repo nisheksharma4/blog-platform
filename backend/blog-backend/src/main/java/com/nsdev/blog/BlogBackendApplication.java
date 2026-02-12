c72b94ff59204f012a11df4167bdbeaa3914a85a
Nishek Sharma <sharmanishek@yahoo.com>
2026-02-12 10:30:20 +0530

    Startup line

--- backend/blog-backend/src/main/java/com/nsdev/blog/BlogBackendApplication.java
+++ backend/blog-backend/src/main/java/com/nsdev/blog/BlogBackendApplication.java
@@ -8,6 +8,7 @@ public class BlogBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogBackendApplication.class, args);
+		System.out.println("Application is Started :) !!!");
 	}
 
 }
